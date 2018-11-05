8faf858122a572cdbe4844bcc2640f0b35da877b
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2018-11-05 12:59:42 +0800

    Update hackerRank: Update day01 of 30daysOfCode

---
+++ hackerRank/30daysOfCode/day01/data-types.js
@@ -0,0 +1,11 @@
+var i2;
+var d2;
+var s2;
+
+i2 = parseInt(readLine());
+d2 = parseFloat(readLine());
+s2 = readLine();
+
+console.log(i + i2);
+console.log((d + d2).toFixed(1));
+console.log(s + s2);
