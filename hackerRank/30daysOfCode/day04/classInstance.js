6a251384e390894197a6dd356c99de5165e35ead
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2018-11-08 23:20:05 +0800

    Complete day04 of Code

--- hackerRank/30daysOfCode/day04/classInstance.js
+++ hackerRank/30daysOfCode/day04/classInstance.js
@@ -1,3 +1,56 @@
+function Person(initialAge) {
+    // Add some more code to run some checks on initialAge
+    var age = initialAge;
+    this.amIOld = function () {
+        // Do some computations in here and print out the correct statement to the console
+        if (age < 13 && age > -1)
+            console.log("You are young.");
+        else if (age < 18 && age > 12)
+            console.log("You are a teenager.");
+        else if (age > 17)
+            console.log("You are old.");
+        else {
+            console.log("Age is not valid, setting age to 0.")
+            age = 0;
+            this.amIOld();
+        }
+    };
+    this.yearPasses = function () {
+        // Increment the age of the person in here
+        age++;
+    };
+}
+
+
+/*
+function Person(initialAge) {
+    // Add some more code to run some checks on initialAge
+    var age;
+    if (initialAge >= 0) {
+        age = initialAge
+    } else {
+        age = 0;
+        console.log('Age is not valid, setting age to 0.');
+    }
+    this.amIOld = function () {
+        // Do some computations in here and print out the correct statement to the console
+        if (age < 13) {
+            console.log('You are young.');
+        } else if (age >= 13 && age < 18) {
+            console.log('You are a teenager.');
+        } else {
+            console.log('You are old.');
+        }
+    };
+    this.yearPasses = function () {
+        // Increment the age of the person in here
+        age += 1;
+    };
+}
+*/
+
+
+/*
 function Person(initialAge){
     // Add some more code to run some checks on initialAge
     this.age = this.age || undefined;
@@ -25,3 +78,4 @@ function Person(initialAge){
        this.age += 1;
    };
 }
+*/
