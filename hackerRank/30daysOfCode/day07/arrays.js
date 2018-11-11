036b379fcc16ec032e9e7824c34449fb7ddf85b3
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2018-11-11 18:22:30 +0800

    Complete day07 of Code

--- hackerRank/30daysOfCode/day07/arrays.js
+++ hackerRank/30daysOfCode/day07/arrays.js
@@ -1,1 +1,8 @@
-console.log(arr.reverse().toString().replace(/,/g,' '));
+function main() {
+  const n = parseInt(readLine(), 10);
+
+  const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+  console.log(arr.reverse().join(' '));
+  // console.log(arr.reverse().toString().replace(/,/g, ' '));
+}
