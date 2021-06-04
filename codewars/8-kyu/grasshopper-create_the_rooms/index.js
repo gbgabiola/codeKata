8243238c8684331f6427e3f247aaeb97c677ea62
Genesis Gabiola <gbgabiola@gmail.com>
2021-06-04 19:55:35 +0800

    Add Grasshopper - Create the rooms kata

---
+++ codewars/8-kyu/grasshopper-create_the_rooms/index.js
@@ -0,0 +1,17 @@
+const rooms = {
+  first: {
+    name: 'Saw',
+    description: 'Saw revolves around the Jigsaw Killer (a.k.a. John Kramer), who tortures victims he believes are complacent or guilty, in order to make them appreciate their time on Earth',
+    completed: true
+  },
+  second: {
+    name: 'Hush',
+    description: 'A deaf writer who retreated into the woods to live a solitary life must fight for her life in silence when a masked killer appears in her window.',
+    completed: true
+  },
+  third: {
+    name: 'Cube',
+    description: 'A group of seven frightened strangers who find themselves trapped in a bizarre maze of cubical rooms, with no memory of how they arrived there. Searching for a way out, they soon discover that many rooms contain lethal booby traps, while others are safe.',
+    completed: false
+  }
+};
