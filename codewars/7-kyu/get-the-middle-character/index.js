1274e2e10723f70c99b36006ace24d927bb6ae40
Genesis Gabiola <gbgabiola@gmail.com>
2021-06-21 23:07:58 +0800

    Add another solution

--- codewars/7-kyu/get-the-middle-character/index.js
+++ codewars/7-kyu/get-the-middle-character/index.js
@@ -1,8 +1,18 @@
-const getMiddle = s => {
-  return s.length % 2 === 0
-    ? s.slice(s.length / 2 - 1, s.length / 2 + 1)
-    : s.slice(s.length / 2, s.length / 2 + 1);
-};
+/*
+// Solution 1:
+function getMiddle(s) {
+  if (s.length % 2 === 0) {
+    return s.slice(s.length / 2 - 1, s.length / 2 + 1);
+  }
+  return s.slice(s.length / 2, s.length / 2 + 1);
+}
+
+// Solution 2:
+const getMiddle = s => s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+*/
+
+// Solution 3
+const getMiddle = s => s.slice((s.length - 1) / 2, s.length / 2 + 1);
 
 // Test Case
 getMiddle('test'); // "es"
